44215b811153d9799bec16487da7c1da24ee5ed0
Metin1 <metin3232@yahoo.com>
2022-04-29 22:28:32 +0300

    fix: port

--- client/src/apollo/index.js
+++ client/src/apollo/index.js
@@ -1,8 +1,7 @@
 import { ApolloClient, from, HttpLink, InMemoryCache } from '@apollo/client'
 import { onError } from '@apollo/client/link/error'
 
-const BASE_URL = process.env.API_SERVER_URL || `/graphql`
-console.log(`LL: BASE_URL`, BASE_URL)
+const BASE_URL = process.env.API_SERVER_URL || '/graphql'
 
 const AuthLink = (operation, next) => {
   const token = localStorage.getItem('jwt')
