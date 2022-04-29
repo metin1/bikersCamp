78ddb4a6891b3efe9ea39c7d20dbea8aaf586b7b
Metin1 <metin3232@yahoo.com>
2022-04-29 19:31:59 +0300

    fix: name

--- client/src/shared/layouts/index.tsx
+++ client/src/shared/layouts/index.tsx
@@ -2,45 +2,45 @@ import React from 'react'
 
 import Box from 'components/Box'
 
-import Footer from './Footer'
-import Header from './Header'
+import Footer from './footer'
+import Header from './header'
 
 interface LayoutProps {
   headerDisabled?: boolean
   children: React.ReactNode
 }
 const Layout: React.FC<LayoutProps> = props => {
-	return (
-		<Box display='flex' flex='1' flexDirection='column'>
-			{!props.headerDisabled && (
-				<Box
-					display='flex'
-					width='100%'
-					alignItems='center'
-					height='64px'
-					borderStyle='solid'
-					borderColor='border'
-				>
-					<Header />
-				</Box>
-			)}
+  return (
+    <Box display='flex' flex='1' flexDirection='column'>
+      {!props.headerDisabled && (
+        <Box
+          display='flex'
+          width='100%'
+          alignItems='center'
+          height='64px'
+          borderStyle='solid'
+          borderColor='border'
+        >
+          <Header />
+        </Box>
+      )}
 
-			<Box flex='1' overflow='auto'>
-				{props.children}
-			</Box>
-			<Box
-				display='flex'
-				width='100%'
-				height='64px'
-				justifyContent='center'
-				alignItems='center'
-				borderStyle='solid'
-				borderColor='border'
-			>
-				<Footer />
-			</Box>
-		</Box>
-	)
+      <Box flex='1' overflow='auto'>
+        {props.children}
+      </Box>
+      <Box
+        display='flex'
+        width='100%'
+        height='64px'
+        justifyContent='center'
+        alignItems='center'
+        borderStyle='solid'
+        borderColor='border'
+      >
+        <Footer />
+      </Box>
+    </Box>
+  )
 }
 
 export default Layout
